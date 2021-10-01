12785b8983ff51be85579569926cb08c06d2935b
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2021-10-02 00:19:59 +0900

    Update

--- prk_pipigherkin/keymap.rb
+++ prk_pipigherkin/keymap.rb
@@ -1,16 +1,10 @@
-# Wait until Keyboard class is ready
 while !$mutex
   relinquish
 end
 
-# Initialize a Keyboard
 kbd = Keyboard.new
 
-# Initialize GPIO assign
 kbd.init_pins(
-  #[ 8, 9, 10, 11, 12 ], # row0, row1,... respectively
-  #[ 2, 3, 4, 5, 6, 7 ]  # col0, col1,... respectively
-  # If you put USB port on the right side, use below instead
   [ 12, 11, 10, 9, 8 ],
   [ 7, 6, 5, 4, 3, 2 ]
 )
@@ -28,19 +22,14 @@ kbd.add_layer :raise, %i(
 )
 kbd.add_layer :adjust, %i(
   KC_F1     KC_F2     KC_F3       KC_F4     KC_F5      KC_F6      KC_F7     KC_F8     KC_F9     KC_F10
-  KC_F11    KC_F12    KC_QUOTE    KC_DQUO   KC_MINUS   KC_LEFT    KC_DOWN   KC_UP     KC_RIGHT  KC_DELETE
-  KC_ESCAPE KC_LGUI   KC_LALT     KC_LCTL   UNLOCK     UNLOCK     KC_RCTL   KC_RALT   KC_RGUI   KC_ESCAPE
+  KC_F11    KC_F12    KC_QUOTE    KC_DQUO   KC_MINUS   KC_LEFT    KC_DOWN   KC_UP     KC_RIGHT  FIBONACCI
+  KC_ESCAPE KC_LGUI   KC_LALT     KC_LCTL   UNLOCK     UNLOCK     KC_RCTL   KC_RALT   KC_RGUI   PASSWD
 )
 kbd.add_layer :lower, %i(
   KC_1      KC_2      KC_3        KC_4      KC_5       KC_6       KC_7      KC_8      KC_9      KC_0
   KC_TAB    KC_NO     KC_QUOTE    KC_DQUO   KC_MINUS   KC_GRAVE   KC_TILD   KC_PIPE   KC_COLON  KC_SCOLON
   KC_ESCAPE KC_LGUI   KC_LALT     KC_LCTL   SPC_LOWER  ADJUST     KC_RCTL   KC_RALT   KC_RGUI   KC_RSFT
 )
-#
-#                   Your custom    Keycode or                          Keycode (only modifiers)    Release time     Re-push time
-#                   key name       Array of Keycode                    or Layer Symbol to be held  threshold(ms)    threshold(ms)
-#                                  or Proc                             or Proc which will run      to consider as   to consider as
-#                                  when you click                      while you keep press        `click the key`  `hold the key`
 kbd.define_mode_key :Z_LSFT,    [ :KC_Z,                               :KC_LSFT,                   150,             150 ]
 kbd.define_mode_key :X_LGUI,    [ :KC_X,                               :KC_LGUI,                   150,             150 ]
 kbd.define_mode_key :C_LALT,    [ :KC_C,                               :KC_LALT,                   150,             150 ]
@@ -54,8 +43,39 @@ kbd.define_mode_key :BSP_LOWER, [ :KC_BSPACE,                          :lower,  
 kbd.define_mode_key :ADJUST,    [ Proc.new { kbd.lock_layer :adjust }, :KC_LSFT,                   300,             nil ]
 kbd.define_mode_key :UNLOCK,    [ Proc.new { kbd.unlock_layer },       :KC_LSFT,                   300,             nil ]
 
-# Tip: You can also switch current layer by single click like this:
-#   kbd.define_mode_key :RAISE, [ Proc.new { kbd.raise_layer }, :KC_LSFT, 150, nil ]
-#   kbd.define_mode_key :LOWER, [ Proc.new { kbd.lower_layer }, :KC_LSFT, 150, nil ]
+class Fibonacci
+  def initialize
+    @a = 0 ; @b = 1
+  end
+  def take
+    result = @a + @b
+    @a = @b
+    @b = result
+  end
+end
+fibonacci = Fibonacci.new
+kbd.define_mode_key :FIBONACCI, [ Proc.new { kbd.macro fibonacci.take }, :KC_NO, 300, nil ]
+
+class Password
+  def initialize
+    @c = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_!@#$%^&*()=-+/[]{}<>'
+  end
+  def generate
+    unless @srand
+      # generate seed with board_millis
+      srand(board_millis)
+      @srand = true
+    end
+    password = ""
+    while true
+      i = rand % 100
+      password << @c[i].to_s
+      break if password.length == 8
+    end
+    return password
+  end
+end
+password = Password.new
+kbd.define_mode_key :PASSWD, [ Proc.new { kbd.macro password.generate, [] }, :KC_NO, 300, nil ]
 
 kbd.start!
